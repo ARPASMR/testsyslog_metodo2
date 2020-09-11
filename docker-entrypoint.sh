4ed4d6b72333ac7158f580eb12dc14a107ed3b4d
S-Grasso <59735407+S-Grasso@users.noreply.github.com>
2020-09-11 13:51:40 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@ while [ true ]; do
 	###########################################################################################
         # Per syslog
         # Per priorità vedere https://www.tutorialspoint.com/unix/unix-system-logging.htm
-	logger --id --stderr -p user.info -t TEST "This is a test message: "`date +"%Y-%m-%d %H:%M:%S"`
-        logger --id --stderr -p user.info -t ALERT "This is an alert message: "`date +"%Y-%m-%d %H:%M:%S"`
+	logger --id --stderr -p user.info -t TEST "Questo è un messaggio di test: "`date +"%Y-%m-%d %H:%M:%S"`
+        logger --id --stderr -p user.alert -t ALERT "Questo è un messaggio di allarme: "`date +"%Y-%m-%d %H:%M:%S"`
         
 	###########################################################################################
 	# E' possibile dopo aver configurato i demone rsyslog locale anche dire a docker, attraverso lo switch della riga di comando "--log-driver syslog" di ridirigere lo stderr
