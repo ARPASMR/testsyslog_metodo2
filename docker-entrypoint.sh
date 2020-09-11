f930d2c55e109bb3257c2c67a8fa6cdb0252753c
S-Grasso <59735407+S-Grasso@users.noreply.github.com>
2020-09-11 12:18:39 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-SYSLOG_MASTER='10.10.0.15'
-SYSLOG_PORT='514'
-
 while [ true ]; do
 	###########################################################################################
-	# Uso di logger senza aver configurato il demone rsyslog locale
-	# in modo da inoltrare i messaggi di log direttamente al MASTER $RSYSLOG_MASTER
-	logger --id --stderr --server $SYSLOG_MASTER -P $SYSLOG_PORT -T -p user.info -t TEST "This is a test message: "`date +"%Y-%m-%d %H:%M:%S"`
-
+        # Per syslog
+        # Per priorità vedere https://www.tutorialspoint.com/unix/unix-system-logging.htm
+	logger --id --stderr -p user.info -t TEST "This is a test message: "`date +"%Y-%m-%d %H:%M:%S"`
+        logger --id --stderr -p user.alert -t ALERT "This is an alert message: "`date +"%Y-%m-%d %H:%M:%S"`
+        
 	###########################################################################################
-	# E' possibile dopo aver configurato i demone rsyslog locale anche dire a docker,
-	# attraverso lo switch della riga di comando "--log-driver syslog" di ridirigere lo stderr
+	# E' possibile dopo aver configurato i demone rsyslog locale anche dire a docker, attraverso lo switch della riga di comando "--log-driver syslog" di ridirigere lo stderr
         # del container all'rsyslog locale all'host ottenendo lo stesso effetto.
+	## echo -e "${DATETIME} : PID $$ : INFO : Questo è un messaggio di test"
+	## echo "This is stdout"
+	## echo "This is stderr" 1>&2
 	### echo "Questo è un messaggio di test: "`date +"%Y-%m-%d %H:%M:%S"`
 	sleep 60
+      
+        #"1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
+        #"2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task."
+        #"3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable."
+        #"4") echo "$D DEBUG This is a debug log that shows a log that can be ignored."
 done
