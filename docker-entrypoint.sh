b2f7a40423a9f260caadc0dd293cb8ddaf71eaee
S-Grasso <59735407+S-Grasso@users.noreply.github.com>
2020-09-11 14:26:38 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,16 +4,16 @@ while [ true ]; do
 	###########################################################################################
         # Per syslog
         # Per priorità vedere https://www.tutorialspoint.com/unix/unix-system-logging.htm
-	logger --id --stderr -p user.info -t TEST "Questo è un messaggio di test: "`date +"%Y-%m-%d %H:%M:%S"`
-        logger --id --stderr -p user.alert -t ALERT "Questo è un messaggio di allarme: "`date +"%Y-%m-%d %H:%M:%S"`
+	#logger --id --stderr -p user.info -t TEST "Questo è un messaggio di test: "`date +"%Y-%m-%d %H:%M:%S"`
+        #logger --id --stderr -p user.alert -t ALERT "Questo è un messaggio di allarme: "`date +"%Y-%m-%d %H:%M:%S"`
         
 	###########################################################################################
 	# E' possibile dopo aver configurato i demone rsyslog locale anche dire a docker, attraverso lo switch della riga di comando "--log-driver syslog" di ridirigere lo stderr
         # del container all'rsyslog locale all'host ottenendo lo stesso effetto.
-	## echo -e "${DATETIME} : PID $$ : INFO : Questo è un messaggio di test"
-	## echo "This is stdout"
-	## echo "This is stderr" 1>&2
-	### echo "Questo è un messaggio di test: "`date +"%Y-%m-%d %H:%M:%S"`
+	echo -e "${DATETIME} : PID $$ : INFO : Questo è un messaggio di test"
+	echo "This is stdout"
+	echo "This is stderr" 1>&2
+	echo "Questo è un messaggio di test: "`date +"%Y-%m-%d %H:%M:%S"`
 	sleep 60
       
         #"1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
